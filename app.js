f2522b34aa2cff395516cf509dc0b446729f79bf
Ermahan <ermahanamanbekov@gmail.com>
2019-11-20 23:59:32 +0600

    feat: form submit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var path = require("path");
+var bp = bodyParser.urlencoded({extended: false});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/message/create', function (req, res) {
+    res.render('create');
+});
+
+app.put('/message/pp', bp, function(req, res) {
+    res.render('show', {data: req.body});
+});
+
+app.listen(3000);
+console.log('Your project is running at 3000 port...');
+
+
+
